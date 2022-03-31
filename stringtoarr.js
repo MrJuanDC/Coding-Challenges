0a9b675fb0661dd257c0bc50370c0684a305b872
Juan Cardona <71093915+MrJuanDC@users.noreply.github.com>
2022-03-31 14:34:43 -0400

    stringtoarray

---
+++ stringtoarr.js
@@ -0,0 +1,14 @@
+// Write a function to split a string and convert it into an array of words.
+
+function stringToArray(string){
+    return string.split(" ");
+  }
+  // P; take in a string
+  // R; return an array of words
+  // E;
+  //       "Robin Singh" ==> ["Robin", "Singh"]
+  // P;
+  //       pass a string into function
+  //       iterate through the string
+  //       on each pass, create a word once we find a space
+  //       return the array with the words separated
